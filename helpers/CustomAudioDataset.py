83493c99464e1d462fcd84162d6786f1120f9b54
Aseer-Ahmad <a3ahmad.kiit@gmail.com>
2023-03-17 17:42:49 +0100

    preprocessor

--- helpers/CustomAudioDataset.py
+++ helpers/CustomAudioDataset.py
@@ -11,12 +11,11 @@ warnings.filterwarnings("ignore")
 
 class CustomAudioDataset(Dataset):
 
-    def __init__(self, audio_dir, sr, transform, max_samples = 50000):
+    def __init__(self, audio_dir, sr, transform):
         self.audio_dir   = audio_dir
         self.audio_files = os.listdir(audio_dir)
         self.transform   = transform
         self.sampling_rate = sr
-        self.max_samples  = max_samples
 
     def __len__(self):
         return len(self.audio_files)
---
+++ helpers/preprocessor.py
@@ -0,0 +1,15 @@
+'''
+class : preprocessor.py
+description : all methods should return a torch vector
+'''
+import torch
+
+def transformByTruncate(audio_vec):
+    MAX_SAMPLES = 50000
+    
+
+def transformByInterpolate(audio_vec):
+    pass
+
+def transformByEmbeddings(audio_vec):
+    pass
